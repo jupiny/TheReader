3ba5e91ddeaeaf4097cae2db50292e1caa9bfaf3
jupiny <tmdghks584@gmail.com>
2017-04-27 19:12:17 +0900

    Add make_mp3_file util function

---
+++ thereader/thereader/utils.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import urllib.request
+
+
+def make_mp3_file(text, speaker, speed):
+    enc_text = urllib.parse.quote(text)
+    data = "speaker={speaker}&speed={speed}&text={text}".format(
+                speaker='mijin',
+                speed='0',
+                text=enc_text,
+            )
+    url = "https://openapi.naver.com/v1/voice/tts.bin"
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", os.environ.get('CLIENT_ID'))
+    request.add_header("X-Naver-Client-Secret", os.environ.get('CLIENT_SECRET'))
+    response = urllib.request.urlopen(request, data=data.encode('utf-8'))
+    rescode = response.getcode()
+    if(rescode == 200):
+        print("TTS mp3 저장")
+        response_body = response.read()
+        with open('1111.mp3', 'wb') as f:
+            f.write(response_body)
+    else:
+        print("Error Code:" + rescode)
