50bfa1aad9fa5b84b18c2142f93be2724e7555a1
jupiny <tmdghks584@gmail.com>
2017-04-28 17:01:43 +0900

    Add media settings

--- thereader/thereader/settings/base.py
+++ thereader/thereader/settings/base.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -120,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media files
+
+MEDIA_ROOT = os.path.join(
+    PROJECT_ROOT_DIR,
+    "dist",
+    "media",
+)
+
+MEDIA_URL = '/media/'
