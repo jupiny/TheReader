c7c32a9f12a5ff02d564131206f3860e4272e6c0
jupiny <tmdghks584@gmail.com>
2017-05-02 20:09:49 +0900

    Keep context in home view using session

--- thereader/thereader/views.py
+++ thereader/thereader/views.py
@@ -4,12 +4,22 @@ from django.shortcuts import redirect
 
 from .utils import make_mp3_file
 
-
+    
 @require_http_methods(["GET"])
 def home(request):
+    text = request.session.pop('text', '')
+    filename = request.session.pop('filename', '')
+    speaker = request.session.pop('speaker', '')
+    speed = request.session.pop('speed', '')
     return render(
         request,
         'index.html',
+        context={
+            'text': text,
+            'filename': filename,
+            'speaker': speaker,
+            'speed': speed,
+        },
     )
 
 
@@ -19,5 +29,11 @@ def make_mp3_file_view(request):
         text = request.POST['text']
         speaker = request.POST['speaker']
         speed = request.POST['speed']
-        make_mp3_file(text, speaker, speed)
+
+        filename = make_mp3_file(text, speaker, speed)
+
+        request.session['text'] = text
+        request.session['filename'] = filename
+        request.session['speaker'] = speaker
+        request.session['speed'] = speed
         return redirect('home')
